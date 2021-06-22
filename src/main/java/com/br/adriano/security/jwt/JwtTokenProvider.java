891570c5bd481e7e7cc1dc4735f5b82746d303bb
Adriano-Cardoso <77011351+Adriano-Cardoso@users.noreply.github.com>
2021-06-21 22:12:15 -0300

    Implements JWT

--- src/main/java/com/br/adriano/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/br/adriano/security/jwt/JwtTokenProvider.java
@@ -1,5 +1,14 @@
 package com.br.adriano.security.jwt;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class JwtTokenProvider {
 
+	
+	@Value()
+	private String secretKey = "secret";
+	
+	private long validityInMilliseconds = 3600000;
 }
